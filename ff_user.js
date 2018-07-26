5af6b33107de8e5015b3a14fcc1ecf84eeac5c7a
d3v00ps <d3v00ps@users.noreply.github.com>
2018-07-25 23:39:41 -0400

    Reformatted for readability.

--- ff_user.js
+++ ff_user.js
@@ -26,6 +26,29 @@
  *   user_pref("", );
  */
 
+// Personal preferences
+//   * Set default homepage
+user_pref("browser.startup.homepage", "https://www.duckduckgo.com");
+//   * Disable page thumbnails capturing
+user_pref("browser.pagethumbnails.capturing_disabled", true);
+//   * Disable new tab activity stream
+user_pref("browser.newtabpage.activity-stream.enabled", false);
+//   * Pressing backspace goes back one page
+user_pref("browser.backspace_action", 0);
+//   * Disable studies & experiments
+user_pref("app.shield.optoutstudies.enabled", false);
+user_pref("experiments.enabled", false);
+user_pref("extensions.shield-recipe-client.enabled", false);
+//   * Disable Pocket
+user_pref("extensions.pocket.enabled", false);
+//   * Display "Insecure Connection" when visiting a site over http
+user_pref("security.insecure_connection_text.enabled", true);
+//   * Disable browser animations
+user_pref("toolkit.cosmeticAnimations.enabled", false);
+//   * Prevent YouTube from loading slowly in Firefox
+user_pref("general.useragent.override.youtube.com", "Mozilla/5.0 (Windows NT 6.1; WOW64; rv:41.0) Gecko/20100101 Firefox/41.0");
+
+// Privacy settings
 user_pref("privacy.firstparty.isolate", true);
 user_pref("privacy.resistFingerprinting", true); // changes your user agent, which may affect downloading add-ons. check about:support
 user_pref("privacy.trackingprotection.enabled", false); // 'false' if using ublock origin, else 'true'
@@ -44,20 +67,3 @@ user_pref("network.http.referer.XOriginPolicy", 2);
 user_pref("network.http.referer.XOriginTrimmingPolicy", 2);
 user_pref("webgl.disabled", true);
 user_pref("signon.rememberSignons", false);
-user_pref("browser.backspace_action", 0); // pressing backspace goes back a page
-// Disable studies & experiments
-user_pref("app.shield.optoutstudies.enabled", false);
-user_pref("experiments.enabled", false);
-user_pref("extensions.shield-recipe-client.enabled", false);
-user_pref("extensions.pocket.enabled", false);
-// Disable page thumbnails capturing
-user_pref("browser.pagethumbnails.capturing_disabled", true);
-// Disable new tab activity stream
-user_pref("browser.newtabpage.activity-stream.enabled", false);
-// Set default homepage
-user_pref("browser.startup.homepage", "https://www.duckduckgo.com");
-// Prevent YouTube from loading slowly in Firefox
-user_pref("general.useragent.override.youtube.com", "Mozilla/5.0 (Windows NT 6.1; WOW64; rv:41.0) Gecko/20100101 Firefox/41.0");
-// Display "Insecure Connection" when visiting a site over http
-user_pref("security.insecure_connection_text.enabled", true);
-user_pref("toolkit.cosmeticAnimations.enabled", false);
