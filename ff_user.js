d5359492b2978094970210a85c2c403b01eedcf4
blankinsec <blankinsec@users.noreply.github.com>
2018-09-03 01:31:51 -0400

    Added tweaks from privacytools.io

--- ff_user.js
+++ ff_user.js
@@ -16,19 +16,18 @@
  *   1. Go to about:support, next to 'Profile Folder' click Open Folder.
  *   2. Delete this file, 'user.js'.
  *   3. Completely close and restart Firefox browser.
- * Sources:
+ * All sources:
  *   https://wiki.mozilla.org/Privacy/Privacy_Task_Force/firefox_about_config_privacy_tweeks
  *   https://www.privacytools.io/#about_config
  *   https://www.reddit.com/r/firefox/comments/7kvk0b/configuring_firefox_for_better_security_privacy/
  *   https://www.reddit.com/r/firefox/comments/91hbkw/youtube_page_load_is_5x_slower_in_firefox_and/
  *   https://www.reddit.com/r/firefox/comments/91kebu/psa_newest_chrome_marks_all_http_not_secure_learn/
+ *   https://www.reddit.com/r/firefox/comments/91t66x/whats_the_best_aboutconfig_settings_that_you/
  * Template:
  *   user_pref("", );
  */
 
 // Personal preferences
-//   * Set default homepage
-user_pref("browser.startup.homepage", "https://www.duckduckgo.com");
 //   * Disable page thumbnails capturing
 user_pref("browser.pagethumbnails.capturing_disabled", true);
 //   * Disable new tab activity stream
@@ -51,11 +50,15 @@ user_pref("general.useragent.override.youtube.com", "Mozilla/5.0 (Windows NT 6.1
 // Privacy settings
 user_pref("privacy.firstparty.isolate", true);
 user_pref("privacy.resistFingerprinting", true); // changes your user agent, which may affect downloading add-ons. check about:support
-user_pref("privacy.trackingprotection.enabled", false); // 'false' if using ublock origin, else 'true'
+user_pref("privacy.trackingprotection.enabled", true); // this may or may not matter if you're using uBlock Origin
 user_pref("browser.cache.offline.enable", false);
+user_pref("browser.safebrowsing.malware.enabled", true); // I would rather go w/ the sec benefit here
+user_pref("browser.safebrowsing.phishing.enabled", true); // I would rather go w/ the sec benefit here
 user_pref("browser.send_pings", false);
-user_pref("browser.sessionstore.max_tabs_undo", 3);
+user_pref("browser.sessionstore.max_tabs_undo", 0);
 user_pref("browser.urlbar.speculativeConnect.enabled", false);
+user_pref("media.eme.enabled", false); // disables DRM playback, but does not apply to Flash or Silverlight if you have those installed (TN: don't install them)
+user_pref("media.gmp-widevinecdm.enabled", false); // disables more DRM content
 user_pref("dom.battery.enabled", false);
 user_pref("dom.event.clipboardevents.enabled", false);
 user_pref("geo.enabled", false);
@@ -67,3 +70,5 @@ user_pref("network.http.referer.XOriginPolicy", 2);
 user_pref("network.http.referer.XOriginTrimmingPolicy", 2);
 user_pref("webgl.disabled", true);
 user_pref("signon.rememberSignons", false);
+user_pref("browser.sessionstore.privacy_level", 2); // never store extra session data
+user_pref("network.IDN_show_punycode", true); // prevent phishing attacks that use punycode chars
